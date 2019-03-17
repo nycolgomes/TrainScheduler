3ef153ea1f5142c6d7f1f5a9c79a511b0a5fd329
NycolGomes <nycolgomes@gmail.com>
2019-03-16 20:17:29 -0400

    everything but firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,19 @@
-// FIREBASE
-// --------------------
-// config Here
-// --------------------
+//refence from another assignment 
+// --------------------------------------------------------------------------------------------------
+// // Initialize Firebase
+// var config = {
+//     apiKey: "AIzaSyCKM1ma3uvpBmwweUkTEw7sqYmJf6q2IG0",
+//     authDomain: "employee-data-60276.firebaseapp.com",
+//     databaseURL: "https://employee-data-60276.firebaseio.com",
+//     projectId: "employee-data-60276",
+//     storageBucket: "employee-data-60276.appspot.com",
+//     messagingSenderId: "1091434908933"
+// };
+// firebase.initializeApp(config);
 
-var trainData = firebase.database();
+// //create a varible to reference the firebase
+// var trainData = firebase.database();
+//--------------------------------------------------------------------------------------------------
 
 $("#addTrainBtn").on("click",function(){
     var trainTime = $("#trainTimeInput").val().trim();
@@ -25,5 +35,12 @@ trainData.ref().on("child_added", function(snapshot){
 
     var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
     var minutes = frequency - remainder;
-    
+    var arrival = moment().add(minutes, "m").format("hh:mm A")
+
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival); 
+
+    $("#trainTable > tBody").append("<tr><td>"+name +"</td><td>"+destination 
+    +"</td><td>"+frequency +"</td><td>"+arrival +"</td><td>"+minutes +"</td></tr>")
 })
